8209bf082fbc3f2326050d0d2ae2872524c4c6f8
irisreitsmaRQ <iris.reitsma@riskquest.com>
2023-07-17 16:09:20 +0200

    update imports

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import plotly.express as px 
-import plotly.graph_objects as go
-import pandas as pd
-import matplotlib.pyplot as plt
+# import plotly.express as px 
+# import plotly.graph_objects as go
+# import pandas as pd
+# import matplotlib.pyplot as plt
 
 
 
