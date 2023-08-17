5c17551191a2a32a545fef47b0712ec2f2d1e62a
irisreitsmaRQ <iris.reitsma@riskquest.com>
2023-08-17 16:09:42 +0200

    Improved code readability by adding docstrings and function signatures

--- dashboard.py
+++ dashboard.py
@@ -6,49 +6,129 @@ import gspread
 from google.oauth2 import service_account
 
 
-def generate_help(options, examples):
-    help_string = 'These are examples of the answers: \n\n' 
-    for n, t in enumerate(options):
-        help_string += '**'
-        help_string += t
-        help_string += '**'
-        help_string += ': '
-        help_string += examples[n]
-        help_string += '\n\n'
-    
-    return help_string
-    
+@st.cache_data
+def load_images(image_path1: str, image_path2: str, image_path3: str) -> None:
+    """
+    Load and display images in a formatted layout.
 
-def riskDriver(riskdriver_groupby, riskdriver, number):
+    This function loads and displays three images from provided file paths in a
+    structured layout using Streamlit's column layout features.
 
+    Args:
+        image_path1 (str): File path to the first image.
+        image_path2 (str): File path to the second image.
+        image_path3 (str): File path to the third image.
+
+    Returns:
+        None
+    """
+    # Open images
+    image_RQ = Image.open(image_path1)
+    image_C8 = Image.open(image_path2)
+    image_CF = Image.open(image_path3)
+
+    # Place images in nice layout in columns
+    col1, col2, col3 = st.columns([2,4,2])
+    with col2:
+        st.image(image_CF)
+    col1, col2, col3 = st.columns([10, 1, 7]) 
+    with col1:
+        st.image(image_RQ)
+    with col3:
+        st.image(image_C8)
+
+    return
+
+
+@st.cache_data
+def load_expert_data(filepath_weights: str) -> pd.DataFrame:
+    """
+    Load expert data from an Excel file.
+
+    This function reads an Excel file containing expert weights data and returns
+    the data as a pandas DataFrame.
+
+    Args:
+        filepath_weights (str): File path to the Excel file containing expert weights data.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the loaded expert weights data.
+    """
+    weights_df = pd.read_excel(filepath_weights)
+
+    return weights_df
+
+
+@st.cache_data
+def load_drivers_data(filepath_risk: str) -> pd.DataFrame:
+    """
+    Load risk drivers data from an Excel file.
+
+    This function reads an Excel file containing risk drivers data and returns
+    the data as a pandas DataFrame. 
+
+    Args:
+        filepath_risk (str): File path to the Excel file containing risk drivers data.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the loaded risk drivers data with missing
+        values filled using forward fill.
+    """
+    drivers_df = pd.read_excel(filepath_risk)
+    drivers_df = drivers_df.fillna(method='ffill', axis=0)
+
+    return drivers_df
+
+    
+def riskDriver(riskdriver_groupby: pd.DataFrame, riskdriver: str, number: int) \
+    -> tuple[float, list[float], float, str]:
+    """
+    Calculate risk driver scores and provide user interface for customization.
+
+    This function calculates risk driver scores based on user inputs for various
+    variables. It presents an interactive user interface for selecting options
+    and overwriting scores for each risk driver.
+
+    Args:
+        riskdriver_groupby (pd.DataFrame): Grouped data containing risk driver information.
+        riskdriver (str): Name of the risk driver.
+        number (int): Sequential number of the risk driver.
+
+    Returns:
+        tuple[float, list[float], float, str]: A tuple containing:
+            - The calculated normalized score for the risk driver.
+            - A list of normalized scores for variables.
+            - The sum of variable scores.
+            - The selected overwrite option for the risk driver score.
+    """
     # Define column width
     col1, col2, col3 = st.columns([8, 1, 3])
 
     # Retrieve driver information from groupby object
-    subvariables = riskdriver_groupby['Variable'].unique().tolist()
+    variables = riskdriver_groupby['Variable'].unique().tolist()
     overwrites = riskdriver_groupby['Overwrites'].unique().tolist()
     overwrites.insert(0, 'No overwrite')
 
     # Create drop down for riskdriver
     with col1:
         with st.expander('Risk Driver ' + str(number) + ': ' + riskdriver):
-            inputs = [None]*len(subvariables)
+            inputs = [None]*len(variables)
 
             # Loop over variables
-            for i, sv in enumerate(subvariables):
-                options = riskdriver_groupby[riskdriver_groupby['Variable']==sv]['Answers'].tolist()
-                examples = riskdriver_groupby[riskdriver_groupby['Variable']==sv]['Example'].tolist()
+            for i, v in enumerate(variables):
+                options = riskdriver_groupby[riskdriver_groupby['Variable']==v]['Answers'].tolist()
+                examples = riskdriver_groupby[riskdriver_groupby['Variable']==v]['Example'].tolist()
                 string = generate_help(options, examples)
-                choice = st.selectbox(sv, options, help=string)
+                choice = st.selectbox(v, options, help=string)
 
-                # Determine score for subvariable (sv)
-                sv_score = options.index(choice) + 1
-                sv_score_rel = sv_score / len(options)
-                inputs[i] = sv_score_rel
+                # Determine score for variable (v)
+                v_score = options.index(choice) + 1
+                v_score_rel = v_score / len(options)
+                inputs[i] = v_score_rel
 
             # Determince score for risk driver
             score = sum(inputs)
-            score_rel = score / len(subvariables)
+            score_rel = score / len(variables)
 
     # Display score for risk driver based on score per varialbe
     with col2:
@@ -64,8 +144,51 @@ def riskDriver(riskdriver_groupby, riskdriver, number):
     return score_rel, inputs, sum(inputs), overwrite
 
 
-def convert_range(value, min_original, max_original, min_new, max_new):
+def generate_help(options: list[str], examples: list[str]) -> str:
+    """
+    Generate a help string with examples for each option.
 
+    This function generates a formatted help string containing examples for each
+    option provided. The options and their corresponding examples are combined
+    into a user-friendly format.
+
+    Args:
+        options (list[str]): A list of options for a variable.
+        examples (list[str]): A list of examples corresponding to each option.
+
+    Returns:
+        str: A formatted help string containing option names and examples.
+    """ 
+    help_string = 'These are examples of the answers: \n\n' 
+    for n, t in enumerate(options):
+        help_string += '**'
+        help_string += t
+        help_string += '**'
+        help_string += ': '
+        help_string += examples[n]
+        help_string += '\n\n'
+    
+    return help_string
+
+
+def convert_range(value: float, min_original: float, max_original: float, \
+                  min_new: float, max_new:float) -> float:
+    """
+    Convert a value from one range to another range.
+
+    This function takes a value within a specified original range and scales
+    it to a new range defined by the given minimum and maximum values.
+
+    Args:
+        value (float): The value to be converted.
+        min_original (float): The minimum value of the original range.
+        max_original (float): The maximum value of the original range.
+        min_new (float): The minimum value of the new range.
+        max_new (float): The maximum value of the new range.
+
+    Returns:
+        float: The scaled value within the new range.
+    """
     # Calculate the range of the original values
     range_original = max_original - min_original
 
@@ -76,8 +199,26 @@ def convert_range(value, min_original, max_original, min_new, max_new):
 
 
 @st.cache_data
-def prepare_weights(weights_df, finance_weight, risk_weight, invest_weight, busdev_weight):
+def prepare_weights(weights_df: pd.DataFrame, finance_weight: bool, risk_weight: bool, 
+                    invest_weight: bool, busdev_weight: bool) -> pd.DataFrame:
+    """
+    Prepare weighted scores based on selected expert groups.
 
+    This function prepares the weights for different riskdrivers based on 
+    user-selected expert groups. It calculates the weighted average of the selected 
+    expert groups and returns the resulting weights for further analysis.
+
+    Args:
+        weights_df (pd.DataFrame): DataFrame containing expert groups and their scores.
+        finance_weight (bool): If True, include Finance expert group in calculations.
+        risk_weight (bool): If True, include Risk expert group in calculations.
+        invest_weight (bool): If True, include Invest expert group in calculations.
+        busdev_weight (bool): If True, include Business Development expert group in calculations.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing calculated weights for each risk driver.
+    """
+   
     # Retrieve which weights are selected
     selected_groups = []
     if finance_weight:
@@ -89,11 +230,9 @@ def prepare_weights(weights_df, finance_weight, risk_weight, invest_weight, busd
     if busdev_weight:
         selected_groups.append('Sub score Bus Dev')
 
-    # Calculate the average of the selected weights
+    # Calculate the weighted average of the selected weights
     selected_weights_df = weights_df[selected_groups]
     weights_df['Sum'] = selected_weights_df.sum(axis=1)
-
-
     total_points = weights_df['Sum'].sum()
     weights_df['Weight'] = weights_df['Sum'] / total_points
 
@@ -102,51 +241,22 @@ def prepare_weights(weights_df, finance_weight, risk_weight, invest_weight, busd
     return weights_df 
 
 
-@st.cache_data
-def load_expert_data(filepath_weights):
-    weights_df = pd.read_excel(filepath_weights)
+def create_weights_UI() -> tuple[bool, bool, bool, bool]:
+    """
+    Create a user interface for choosing expert weights.
 
-    return weights_df
+    This function generates a user interface with checkboxes for selecting expert
+    weights from different expert groups. It allows users to choose which expert
+    groups to include in the calculations.
 
+    Returns:
+        tuple[bool, bool, bool, bool]: A tuple containing Boolean values indicating whether
+        each of the expert weights (Finance, Risk, Invest, Bus Dev) is selected.
 
-@st.cache_data
-def load_drivers_data(filepath_risk):
-    drivers_df = pd.read_excel('RD_test.xlsx')
-    drivers_df = drivers_df.fillna(method='ffill', axis=0)
-
-    return drivers_df
-
-
-def apply_weights(weights_df, risk_driver, value):
-
-    # Obtain and apply weight
-    weight = weights_df.loc[weights_df['Risk Factor'] == risk_driver, 'Weight'].values[0]
-    weighted_value = value * weight
-
-    return weighted_value
-
-
-@st.cache_data
-def load_images(image_path1, image_path2, image_path3):
-    image_RQ = Image.open(image_path1)
-    image_C8 = Image.open(image_path2)
-    image_CF = Image.open(image_path3)
-
-    col1, col2, col3 = st.columns([2,4,2])
-
-    with col2:
-        st.image(image_CF)
-    col1, col2, col3 = st.columns([10, 1, 7]) 
-    with col1:
-        st.image(image_RQ)
-    with col3:
-        st.image(image_C8)
-
-    return
-
-
-def create_weights_UI():
+    """
     st.header('Choose expert weights')
+
+    # Create checkboxes for each expert group
     col1, col2, col3, col4 = st.columns([1,1,1,1])
     with col1:
         finance_weight = st.checkbox('Finance', value=True)
@@ -156,12 +266,38 @@ def create_weights_UI():
         invest_weight = st.checkbox('Invest', value=True)
     with col4:
         busdev_weight = st.checkbox('Bus Dev', value=True)
+
+    # At least one of the weights should be selected, display error if not
     if not any([finance_weight, risk_weight, invest_weight, busdev_weight]):
         st.error('Please select at least one expert weight', icon="🚨")
 
     return finance_weight, risk_weight, invest_weight, busdev_weight
 
 
+def apply_weights(weights_df: pd.DataFrame, risk_driver: str, value: float) -> float:
+    """
+    Apply a weight to a given value based on a specific risk driver.
+
+    This function applies a weight from a DataFrame of weights to a provided risk 
+    driver score. It calculates the weighted value using the weight associated 
+    with the given risk driver.
+
+    Args:
+        weights_df (pd.DataFrame): DataFrame containing risk driver weights.
+        risk_driver (str): Name of the risk driver for which the weight should be applied.
+        value (float): The value to be weighted.
+
+    Returns:
+        float: The value after applying the specified weight.
+    """
+    # Obtain and apply weight
+    weight = weights_df.loc[weights_df['Risk Factor'] == risk_driver, 'Weight'].values[0]
+    weighted_value = value * weight
+
+    return weighted_value
+
+
+############################################################################################################
 if __name__ == '__main__':
 
     # Create logo header
