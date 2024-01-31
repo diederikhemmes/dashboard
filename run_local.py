7d4299b9847907f253fa81f8b7010f721fe240d3
diederikhemmes <diederik.hemmes@riskquest.com>
2024-01-31 21:45:13 +0100

    second try scorecard v 0.96

--- run_local.py
+++
@@ -1,9 +0,0 @@
-import sys
-import streamlit
-from streamlit.web import cli as stcli
-
-if __name__ == '__main__':
-    sys.argv = ["streamlit", "run", "dashboard.py"]
-    sys.exit(stcli.main())
-
-
